6741354a13bffc055964ed24d0336fed09fd67b7
Susannabency1999 <bencysusanna1999@gmail.com>
2026-08-10 16:53:58 +0530

    update changes

--- 2_table.js
+++
@@ -1,5 +0,0 @@
-for (count=1;count<=10; count++)
-{
-    var num =count *2;
-    console.log(count,"* 2 = ", num);
-}
--- vowel.js
+++
@@ -1,9 +0,0 @@
-var l="b";
-var vow="a,e,i,o,u";
-
-if (vow.includes(l)) {
-    console.log("vowel");   
-}
-else{
-    console.log("consonant");
-}
